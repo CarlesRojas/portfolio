f4285f1b10b5a13db687298ac81e4f02bb214450
Carles <carles.gstocks@gmail.com>
2020-11-08 10:31:39 +0100

    Sections Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Cursor from "components/Cursor";
 
 export default function App() {
+    // Set dark mode
+    document.body.classList.add("dark");
+
     return (
-        <div className="App">
+        <div className="app">
+            <div className="section"></div>
+            <div className="section red"></div>
+            <div className="section blue"></div>
+            <div className="section green"></div>
             <Cursor />
         </div>
     );
